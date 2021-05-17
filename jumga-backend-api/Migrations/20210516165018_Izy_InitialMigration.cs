99b52a4dce1002587b9e12146dede89357496594
Isreal Ulelu <isrealulelu@Advansios-MacBook-Pro.local>
2021-05-17 11:10:52 +0100

    Features Added: New DB, Docker

---
+++ jumga-backend-api/Migrations/20210516165018_Izy_InitialMigration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace JumgaAPI.Migrations
+{
+    public partial class Izy_InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
